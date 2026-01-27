97262ce2558d018a1b6f7280147b0b8fbf454701
Moshe <chemedstorage@gmail.com>
2026-01-27 22:06:53 +0200

    add: add setting for cookie on ligin user

--- stc/Controllers/AuthController.ts
+++ stc/Controllers/AuthController.ts
@@ -11,12 +11,16 @@ export const loginUser = async (req: Request, res: Response) => {
         _id: user._id!,
         name: user.name!,
         email: user.email!,
-        isAdmin: user.isAdmin
+        isAdmin: user.isAdmin,
       },
       process.env.JWT_SECRET as string,
       { expiresIn: "1d" }
     );
-    res.cookie("token", token, { httpOnly: true });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
     res.json(user).status(200);
   } catch (error) {
     res.status(400).json({ data: null, Error: error });
@@ -25,18 +29,18 @@ export const loginUser = async (req: Request, res: Response) => {
 
 export const logoutUser = (req: Request, res: Response) => {
   res.clearCookie("token");
-  res.json({message:"user is logout"})
+  res.json({ message: "user is logout" });
 };
 
-export const isLoginUser = (req: Request, res: Response)=>{
+export const isLoginUser = (req: Request, res: Response) => {
   try {
     const token = req.cookies.token;
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-    res.status(200).json(decoded) 
+    res.status(200).json(decoded);
   } catch (error) {
-    res.status(400).json(error)
+    res.status(400).json(error);
   }
-}
+};
 
 export const registerUser = async (req: Request, res: Response) => {
   try {
